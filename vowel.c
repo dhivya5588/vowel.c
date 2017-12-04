663671f10a0b61814172e25e137d842aa1526843
dhivya5588 <32833363+dhivya5588@users.noreply.github.com>
2017-12-04 12:51:57 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char let;
+scanf("%c",&let);
+if(let=='a'||let=='e'||let=='i'||let=='o'||let=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("not a vowel");
+}
+getch();
+}
